38458e18b7b640b66b7b7245c5325ece08135bc5
Zohirul Haque <zhremon@outlook.com>
2023-10-11 12:09:54 +0600

    Printing Output in two different ways

---
+++ 5PrintingOutputIn2differentways.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main()
+{
+	// Printing in first way
+	print("Welcome to HollowLand! // printing from print statement");
+
+	// Printing in second way
+	stdout.write("Welcome to HollowLand! // printing from stdout.write()");
+}
