36800fd909076da4cea2e10b0dd0dda19cb20cd8
Zohirul Haque <zhremon@outlook.com>
2023-10-21 09:06:33 +0600

    Variables showing good way

---
+++ 2Variables.dart
@@ -0,0 +1,8 @@
+main() {
+
+  String CharacterName = "Tanjin Tisha";
+  String CharacterAge = "33";
+
+  print("A young beautiful model, ${CharacterName}");
+  print("Who turns ${CharacterAge} years old in this summer being not married");
+}
