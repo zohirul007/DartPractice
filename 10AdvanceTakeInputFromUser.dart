f23d8125dc5279ddc65ed7151a2f723bb37fe787
Zohirul Haque <zhremon@outlook.com>
2023-10-23 08:30:32 +0600

    Advance taking input from user string cnvrt double

--- 10AdvanceTakeInputFromUser.dart
+++ 10AdvanceTakeInputFromUser.dart
@@ -4,9 +4,9 @@ import "dart:io";
 
 void main() {
   print("Enter first number: ");
-  double? num1 = double.parse(stdin.readLineSync()!); // ? & ! null value
+  double? num1 = double.parse(stdin.readLineSync()!); // ? & ! define null safety in double(String)
   print("Enter second number: ");
-  double? num2 = double.parse(stdin.readLineSync()!); // ? & ! null value
+  double? num2 = double.parse(stdin.readLineSync()!); // ? & ! define null safety in double(String)
 
   print(
     num1 + num2
--- 3TakingAstringinputFromUser.dart
+++ 3TakingAstringinputFromUser.dart
@@ -4,7 +4,7 @@ import 'dart:io';
 main() {
   print("What is your name?");
   // Reading name of the User
-  String? name = stdin.readLineSync(); // null safety in name string
+  String? name = stdin.readLineSync(); // ? null safety in name string
   // Printing the name
   print("Hello, $name! \nWelcome to Hollowland!!");
 }
