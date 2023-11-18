0786dd9b88f15a79cd2c88ee815ad77efee373d9
Zohirul Haque <zhremon@outlook.com>
2023-11-18 12:52:31 +0600

    Find grade with if else statement

--- 13find_grade.dart
+++ 13find_grade.dart
@@ -1,12 +1,12 @@
 /*If the student receives over 40% on her test, report that her grade passes;
-if not, report that her grade fails. with else condtion*/
+if not, report that her grade fails. when if condition failed we can find it with else condtion*/
 
 void main() {
   var grade = 39;
 
   if (grade >= 40) {
     print("Your grade is: pass");
-  } else{
+  } else {
     print("Your grade is: Fail");
   }
 }
