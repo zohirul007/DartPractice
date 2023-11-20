4df4f34102e0f1531be4d9dbf303c959e85fab04
Zohirul Haque <zhremon@outlook.com>
2023-11-20 23:20:33 +0600

    conditions in dart

---
+++ 14B_statement.dart
@@ -0,0 +1,12 @@
+/*
+If he has money in his account, calculate interest;
+if he doesn’t, charge a penalty fee
+ */
+
+void main() {
+  var balance = -5;
+
+  if (balance < 0) {
+    print("Your Bank balance is below 0, add funds now or you will be charged a penalty");
+  }
+}
