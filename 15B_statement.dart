1a9976ae0fc4a8fe82465fa17e1aaefb77cc7efb
Zohirul Haque <zhremon@outlook.com>
2023-11-22 09:36:25 +0600

    conditionals

---
+++ 15B_statement.dart
@@ -0,0 +1,14 @@
+/*
+If he has money in his account, calculate interest;
+if he doesn’t, charge a penalty fee
+when if statement fail, use if else statement
+ */
+void main() {
+  var balance = 801;
+
+  if (balance < 0) {
+    print("Your Bank balance is below 0, add funds now or you will be charged a penalty");
+  } else {
+    print("Your Bank Balance is: above 0");
+  }
+}
